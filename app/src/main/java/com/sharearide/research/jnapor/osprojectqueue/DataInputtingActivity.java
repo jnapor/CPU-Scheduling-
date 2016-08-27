e12a47db41760b1c748b1120b4d068b949e13b00
jnapor <apor.johnnichol00@gmail.com>
2016-08-27 13:31:26 +0800

    Added 2 Floating Action Button

--- app/src/main/java/com/sharearide/research/jnapor/osprojectqueue/DataInputtingActivity.java
+++ app/src/main/java/com/sharearide/research/jnapor/osprojectqueue/DataInputtingActivity.java
@@ -1,15 +1,36 @@
 package com.sharearide.research.jnapor.osprojectqueue;
 
-import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DataInputtingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.row_data);
+        setContentView(R.layout.process_gathering_layout);
+//
+//        TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
+        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add_process);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Add Process", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        FloatingActionButton simulate = (FloatingActionButton) findViewById(R.id.simulate);
+        simulate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Simulate", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
