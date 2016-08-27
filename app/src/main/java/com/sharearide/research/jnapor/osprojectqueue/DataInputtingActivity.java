fa6a8673dc19b1e01c3cb89209304bb272bae586
jnapor <apor.johnnichol00@gmail.com>
2016-08-27 19:23:57 +0800

    Onclick event adds a new row

--- app/src/main/java/com/sharearide/research/jnapor/osprojectqueue/DataInputtingActivity.java
+++ app/src/main/java/com/sharearide/research/jnapor/osprojectqueue/DataInputtingActivity.java
@@ -1,26 +1,93 @@
 package com.sharearide.research.jnapor.osprojectqueue;
 
+import android.graphics.Typeface;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DataInputtingActivity extends AppCompatActivity {
+    int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.process_gathering_layout);
-//
-//        TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
+
+        ScrollView scrollView = (ScrollView) findViewById(R.id.scrollView);
+        final TableLayout tableLayout = (TableLayout) scrollView.findViewById(R.id.table);
+
         FloatingActionButton add = (FloatingActionButton) findViewById(R.id.add_process);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(view.getContext(), "Add Process", Toast.LENGTH_SHORT).show();
+                counter++;
+                TableRow tableRow = new TableRow(view.getContext());
+                tableRow.setLayoutParams(new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
 
 
+
+                TextView textView = new TextView(view.getContext());
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                textView.setTypeface(null, Typeface.BOLD);
+                textView.setText(String.valueOf(counter));
+                textView.setTextSize(50.0f);
+
+                TextView textView1 = new TextView(view.getContext());
+                textView1.setText(String.valueOf(counter));
+                textView1.setGravity(Gravity.CENTER);
+                textView1.setTypeface(null, Typeface.BOLD);
+
+                TextView textView2 = new TextView(view.getContext());
+                textView2.setText(String.valueOf(counter));
+                textView2.setGravity(Gravity.CENTER);
+                textView2.setTypeface(null, Typeface.BOLD);
+
+//                EditText editText = new EditText(view.getContext());
+//                editText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+//                editText.setGravity(Gravity.CENTER);
+//                editText.setTypeface(null, Typeface.BOLD);
+//
+//
+//                EditText editText1 = new EditText(view.getContext());
+//                editText1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+//                        LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+//                editText1.setGravity(Gravity.CENTER);
+//                editText1.setTypeface(null, Typeface.BOLD);
+
+
+                tableRow.addView(textView);
+                tableRow.addView(textView1);
+                tableRow.addView(textView2);
+
+//                TextView textView = new TextView(view.getContext());
+//
+//                EditText editText = new EditText(view.getContext());
+//                EditText editText1 = new EditText(view.getContext());
+//
+//                tableRow.addView(getTextView(textView));
+//                tableRow.addView(getEditTextView(editText));
+//                tableRow.addView(getEditTextView(editText1));
+
+//                tableRow.addView(textView);
+//                tableRow.addView(editText);
+//                tableRow.addView(editText1);
+
+                tableLayout.addView(tableRow);
             }
         });
 
@@ -33,4 +100,28 @@ public class DataInputtingActivity extends AppCompatActivity {
         });
 
     }
+
+    private TextView getTextView(TextView textView){
+        counter++;
+        textView.setText(String.valueOf(counter));
+
+        textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+        textView.setGravity(Gravity.CENTER);
+        textView.setTypeface(null, Typeface.BOLD);
+        //textView.setTextAppearance( android.R.style.TextAppearance_Medium);
+
+        return textView;
+    }
+
+    private EditText getEditTextView(EditText editText){
+        editText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT,1.0f));
+        editText.setGravity(Gravity.CENTER);
+        editText.setTypeface(null, Typeface.BOLD);
+        //editText.setTextAppearance(null, android.R.style.TextAppearance_Medium);
+
+        return editText;
+    }
+
 }
